e0c08d5634381bf5aa476684971c342ad1bed331
Rohan Agrawal <Rohanagrawal1798@gmail.com>
2022-12-11 02:39:54 +0530

    dbconnection variable below cors

--- server/app.js
+++ server/app.js
@@ -7,17 +7,13 @@ const cookieParser = require('cookie-parser')
 const cors = require("cors");
 // connecting to database
 
-DBconnection();
 
 //middleware
 app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// app.get('/addtodo', (req, res) => {
-//     res.send('Hello !')
-
-//   })
+DBconnection();
 app.use("/",routes)
 
 
